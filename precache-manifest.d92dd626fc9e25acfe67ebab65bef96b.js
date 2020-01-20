f68d2f0299f85e1dbe9980aea8518e7ae41d5843
archi777 <wowgladarena@mail.ru>
2020-01-20 17:57:54 +0300

    Updates

---
+++ precache-manifest.d92dd626fc9e25acfe67ebab65bef96b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f053f703fa415f80f7ae20d8af137dd",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "58f10ae88f91c4c8741c",
+    "url": "/blog/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "8b1c728cb51a3fc2e4c0",
+    "url": "/blog/static/css/main.af2f07e1.chunk.css"
+  },
+  {
+    "revision": "58f10ae88f91c4c8741c",
+    "url": "/blog/static/js/2.1cdcf1b2.chunk.js"
+  },
+  {
+    "revision": "fabe6e8bc1203e177b62cd9daebbf511",
+    "url": "/blog/static/js/2.1cdcf1b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b1c728cb51a3fc2e4c0",
+    "url": "/blog/static/js/main.b2023936.chunk.js"
+  },
+  {
+    "revision": "5ed9ce27c01511d13c55",
+    "url": "/blog/static/js/runtime-main.202430bf.js"
+  }
+]);
